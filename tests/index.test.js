5c5a2cbf9a421d8e7f5967057f3b66ef2fc562ba
_wbstr <32849589+wbster@users.noreply.github.com>
2021-02-06 14:44:37 +0300

    added remove | clearAll | update | class State | README

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,15 @@
 const q1t = require('../dist')
 
-describe('', () => {
+describe('tests', () => {
 
-	test('onOf', () => {
+	test('oneOf', () => {
 		const summ = q1t({
 			a: 0,
 			b: 0,
 			result: 0
 		})
 
-		summ.onOf(['a', 'b'], ({ a, b }) => {
+		summ.oneOf(['a', 'b'], ({ a, b }) => {
 			summ.set('result', a + b)
 		})
 
@@ -23,5 +23,56 @@ describe('', () => {
 		summ.set('a', 5)
 
 		summ.set('b', 16)
+	})
+
+	test('update multi', () => {
+		const profile = q1t({
+			email: 'we@gmail.com',
+			name: 'Anton',
+			age: 24
+		})
+
+		profile.once('email', (email) => {
+			expect(email).toBe('web@gmail.com')
+		})
+		profile.once('name', (name) => {
+			expect(name).toBe('Max')
+		})
+		profile.once('age', (age) => {
+			expect(age).toBe(25)
+		})
+
+		profile.update({
+			email: 'web@gmail.com',
+			name: 'Max',
+			age: 25
+		})
+	})
+
+	test('remove', () => {
+		const test = q1t({
+			a: false
+		})
+
+		test.on('a', () => { })
+
+		test.remove('a')
+
+		// @ts-ignore
+		expect(test.eventEmitter.eventList['a']).toBe(undefined)
+	})
+
+	test('describe', () => {
+		const test = q1t({
+			a: false
+		})
+
+		const unsubscriber = test.on('a', () => { })
+
+		unsubscriber()
+
+		// @ts-ignore
+		expect(test.eventEmitter.eventList['a'].length).toBe(0)
+
 	})
 })
