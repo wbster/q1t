262ca57fb1d91e635d5045508900436c51bdc7a4
wbster <wencorsman@gmail.com>
2025-11-16 21:31:38 +0300

    refactor: update LocalStorageState to use the key parameter for localStorage operations

--- src/core/LocalStorageState.ts
+++ src/core/LocalStorageState.ts
@@ -1,12 +1,12 @@
 import { State } from "./State"
 
 export class LocalStorageState<T> extends State<T> {
-	constructor(key: string, initialValue: T) {
+	constructor(private key: string, initialValue: T) {
 		super((JSON.parse(localStorage.getItem(key) as any) as T) || initialValue)
 	}
 
 	setValue(value: T) {
 		super.setValue(value)
-		localStorage.setItem("state", JSON.stringify(value))
+		localStorage.setItem(this.key, JSON.stringify(value))
 	}
 }
