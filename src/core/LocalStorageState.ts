be05ec9ec538ce7f576fa44a26f171717f8d1f0e
wbster <wencorsman@gmail.com>
2025-11-18 09:59:47 +0300

    refactor: replace LocalStorageState class with createLocalStorageState function for improved localStorage handling

--- src/core/LocalStorageState.ts
+++ src/core/LocalStorageState.ts
@@ -1,12 +1,17 @@
 import { State } from "./State"
 
-export class LocalStorageState<T> extends State<T> {
-	constructor(private key: string, initialValue: T) {
-		super((JSON.parse(localStorage.getItem(key) as any) as T) || initialValue)
+export const createLocalStorageState = <T>(key: string, initialValue: T) => {
+	const state = new State(initialValue)
+	const value = JSON.parse(localStorage.getItem(key) as any) as T
+
+	state.setValue(value)
+
+	const originalSetValue = state.setValue
+
+	state.setValue = (value) => {
+		originalSetValue(value)
+		localStorage.setItem(key, JSON.stringify(value))
 	}
 
-	setValue(value: T) {
-		super.setValue(value)
-		localStorage.setItem(this.key, JSON.stringify(value))
-	}
+	return state
 }
