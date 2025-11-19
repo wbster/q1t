b3814f7607b28c5b89d3c936db0811fc528d1c4f
wbster <wencorsman@gmail.com>
2025-11-19 10:42:40 +0300

    fix: add alias to vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config"
+import path from "path"
 
 export default defineConfig({
 	testMatch: ["**/*.spec.ts"],
 	watch: false,
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		}
+	}
 })
